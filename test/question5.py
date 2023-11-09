142c11652325533a23db6adea5383b2fe2c90e62
EvelynGMaldonado <evelyn.gmaldonado@gmail.com>
2023-11-09 10:16:36 -0800

    working on question 5

--- test/question5.py
+++ test/question5.py
@@ -33,3 +33,23 @@ Input to program:
 *
 
 '''
+
+#solution accepts five integer inputs
+#solution outputs three sums of input values; convert before calculating sum
+#first output: sum of five inputs maintained as integer values
+#second output: sum of five inputs converted to float values
+#third output: sum of five inputs converted to string values (concatenate)
+
+value1 = int(input("Enter value 1: "))
+value2 = int(input("Enter value 2: "))
+value3 = int(input("Enter value 3: "))
+value4 = int(input("Enter value 4: "))
+value5 = int(input("Enter value 5: "))
+
+integer_sum_value = value1 + value2 + value3 + value4 + value5
+float_sum_value = float(value1) + float(value2) + float(value3) + float(value4) + float(value5)
+string_sum_value = str(value1) + str(value2) + str(value3) + str(value4) + str(value5)
+
+print(f"Integer: {integer_sum_value}")
+print(f"Float: {float_sum_value}")
+print(f"String: {string_sum_value}")
