d14e4f209f3c6aa6d7331ccb7f75de2007b4ff93
EvelynGMaldonado <evelyn.gmaldonado@gmail.com>
2023-11-09 11:41:26 -0800

    working on question 8

--- test/question8.py
+++ test/question8.py
@@ -36,3 +36,32 @@ Input to program:
 *
 
 '''
+
+#temperature >= 212, water state is "Boiling"
+#temperature (115, 211], water state is "Hot"
+#temperature [80, 115], water state is "Warm"
+#temperature [33, 80), water state is "Cold"
+#temperature < 33, water state is "Frozen"
+#temperature = 212, safety comment "Caution: Hot!"
+#temperature < 33, safety comment "Watch out for ice!"
+#solution accepts integer input representing a water temperature
+#solution outputs the water state and potential safety comment based on temperature
+
+temperature_value_check = int(input("Please enter temperature value: "))
+
+
+if temperature_value_check < 33:
+    print("Frozen")
+    print("Watch out for ice!")
+elif 33 <= temperature_value_check < 80:
+    print("Cold")
+elif 80 <= temperature_value_check < 115:
+    print("Warm")
+elif 115 <= temperature_value_check < 211:
+    print("Hot")
+elif temperature_value_check == 212:
+    print("Caution: Hot!")
+elif temperature_value_check >= 212:
+    print("Boiling")
+else:
+    print("the value is not recognized, please try again")
